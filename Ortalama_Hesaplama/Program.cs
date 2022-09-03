21317d38e7802971b4c6c6013a7008cacd697960
musakibar <kibar378@gmail.com>
2022-09-03 16:35:35 +0300

    Ortalama_ve_Ucgen

---
+++ Ortalama_Hesaplama/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+Console.WriteLine("Fibonacci Dizisi için eleman sayısı giriniz");
+int sayi = int.Parse(Console.ReadLine());
+Fibo(sayi);
+
+
+static void Fibo (int sayi)
+{
+    int x = 0;
+    int y = 1;
+    Console.Write(x + " " + y + " ");
+
+    for(int i = 2; i<sayi; i++)
+    {
+        int z = x + y;
+        Console.Write(z + " ");
+        x = y;
+        y = z;
+
+    }
+
+}
