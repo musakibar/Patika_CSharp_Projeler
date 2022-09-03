d67c4a81e1fe508c26c2a66b13e937a4c6325b44
musakibar <kibar378@gmail.com>
2022-09-03 17:07:13 +0300

    Daire_Cizim

---
+++ Daire_Cizme/Program.cs
@@ -0,0 +1,35 @@
+﻿
+double radius;
+double thickness = 0.4;
+char symbol = '*';
+
+do
+{
+    Console.WriteLine("Lütfen dairenin yarı çapını giriniz");
+    if (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+    {
+        Console.WriteLine("radius have to be positive number");
+    }
+}
+while (radius <= 0);
+
+Console.WriteLine();
+double rIn = radius - thickness, rOut = radius + thickness;
+
+for (double y = radius; y >= -radius; --y)
+{
+    for (double x = -radius; x < rOut; x += 0.5)
+    {
+        double value = x * x + y * y;
+        if (value >= rIn * rIn && value <= rOut * rOut)
+        {
+            Console.Write(symbol);
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
+Console.ReadKey();
