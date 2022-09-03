649e3ae0a21dc6c72b9de94390ccff06a8ea56d0
musakibar <kibar378@gmail.com>
2022-09-03 19:21:33 +0300

    KarakterYerDegistirme

---
+++ Karakter_Tersten_Yazdirma/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Write("Cümlenizi Giriniz: ");
+string[] s = Console.ReadLine().Split(' ');
+Console.WriteLine("");
+
+for(int i = 0; i<s.Length; i++)
+{
+    string t = s[i];
+    string yeni = "";
+    string harf = "";
+    for(int i2 = 0; i2<t.Length-1; i2++)
+    {        
+        harf = t[i2+1].ToString();
+        yeni += harf; 
+    }
+        yeni += t[0].ToString();
+        Console.Write(yeni + " ");
+}
