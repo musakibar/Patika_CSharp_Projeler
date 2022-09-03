798a4b34a0d2509e6baf2d1d208876ff085bd286
musakibar <kibar378@gmail.com>
2022-09-03 16:34:41 +0300

    Ortalama_ve_Ucgen

---
+++ Ucgen_Cizme/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+try
+{
+    Console.Write("Üçgen boyutunu giriniz: ");
+    int boyut = int.Parse(Console.ReadLine());
+    Ciz(boyut);
+}
+
+catch(Exception)
+{
+    Console.WriteLine("Lütfen sayı giriniz.");
+
+}
+
+
+static void Ciz (int boyut)
+{
+    for(int x = 1; x<=boyut; x++)
+    {
+        for(int y=boyut; y>x; y--)
+        {
+            Console.Write(" ");
+        }
+
+        for(int z = 1; z<=x; z++)
+        {
+            Console.Write("* ");
+        }
+
+        Console.WriteLine("");
+
+    }   
+
+}
