fd0472bd9c458f43dafc7d618b3e5923a8020e56
musakibar <kibar378@gmail.com>
2022-09-03 20:07:15 +0300

    int_ikili_toplam

---
+++ Integer_Ikili_Toplam/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+metot();
+
+void metot()
+{
+    Console.WriteLine("Aralarında boşluk bırakarak çift adet sayi giriniz: ");
+    string[] s = Console.ReadLine().Split(' ');
+    if(s.Length % 2 == 1)
+    {
+        Console.WriteLine("Çift adet sayı girmediniz!  Tekrar Deneyin");
+        metot();
+    }
+
+    islem(s);
+}
+
+
+void islem(string[] s)
+{
+    for(int i = 0; i<s.Length-1; i=i+2)
+    {
+        int deger1 = int.Parse(s[i]);
+        int deger2 = int.Parse(s[i+1]);
+
+        if(deger1 == deger2)
+        {
+            Console.Write(Math.Pow(deger1+deger2, 2) + " ");
+        }
+        else
+        {
+            Console.Write(deger1 + deger2 + " ");
+        }
+    }
+
+}
+
