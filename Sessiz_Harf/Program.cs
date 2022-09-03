b2170f68b344148bc8062641c153bf08b1a107e1
musakibar <kibar378@gmail.com>
2022-09-03 20:42:05 +0300

    Sessiz Harf

---
+++ Sessiz_Harf/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Kelimeleri giriniz: ");            
+string[] Array = Console.ReadLine().Split();
+KelimeKontrol(Array);
+Console.ReadLine();
+
+
+static void KelimeKontrol(string[] array)
+{
+    foreach (var item in array)
+    {
+        bool def = false;
+        for (int i = 1; i < item.Length; i++)
+        {
+            if (harfKontrol(item[i]) == harfKontrol(item[i - 1]))
+                def = true;
+        }
+        Console.Write(def + " ");
+    }
+
+    bool harfKontrol(char c)
+    {
+        return !"aeıioöuü".ToCharArray().Contains(c);
+    }
+
+}
